754c51841c4f9ce88047323cc2790bffff5d810c
unknown <kaludivine545@gmail.com>
2023-07-21 16:24:38 +0100

    Added Responsiveness

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+// import React from 'react'
 // import image from '../../assets/div.jpeg'
+import { FaRegPaperPlane } from 'react-icons/fa'
 const About = () => {
   return (
     <section className='mx-4 md:mx-16 lg:mx-24  '>
-      <div className='text-center my-24 '>
+      <div className='text-center my-20 '>
         <h1 className='text-5xl'>About me</h1>
         <h3 className='mt-4'>introduction</h3>
-        <article className='flex justify-between gap-12 mt-16 lg:mx-24 md:mx-12 xl:mx-32'>
-          <div className='bg-hero bg-center bg-cover bg-no-repeat w-[400px] h-[250px] rounded-md'></div>
-          <div className='flex  flex-col gap-12 '>
-            <p className='max-w-md text-left'>
+        <article className='flex flex-col justify-center sm:flex-row sm:justify-between gap-4 sm:gap-6 md:gap-12 mx-4 mt-8 md:mt-16 lg:mx-24 md:mx-12 xl:mx-32'>
+          <div className='bg-hero bg-center bg-cover bg-no-repeat w-full  sm:w-[400px] h-[250px] rounded-md '></div>
+          <div className='flex  justify-center items-center  flex-col gap-8 sm:gap-8 lg:gap-8 '>
+            <p className='max-w-md tracking-wide leading-6 sm:text-md md:text-md text-sm sm:text-left'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
               assumenda quaerat quo reprehenderit ut quia voluptates modi
               perferendis, quos eaque. Laboriosam, magni repellendus quo dolorem
               adipisci officia! Facilis incidunt vel et, veniam maxime quos
               sunt.
             </p>
-            <div className='flex justify-between max-w-md text-center'>
+            <div className='flex justify-between w-full tracking-wide sm:text-md md:text-md text-sm leading-6  text-center'>
               <div className='flex flex-col gap-2'>
-                <h2 className='text-3xl font-bold'>02+</h2>
-                <p>
+                <h2 className='text-3xl font-bold text-center'>02+</h2>
+                <p className='text-center'>
                   years <br /> experience
                 </p>
               </div>
@@ -36,6 +37,9 @@ const About = () => {
                 </p>
               </div>
             </div>
+            <button className='p-4  items-center sm:mr-auto rounded-md capitalize justify-center font-bold flex gap-2 text-white bg-blue-300 w-[150px]'>
+              contact me <FaRegPaperPlane />
+            </button>
           </div>
         </article>
       </div>
--- portfolio/src/components/hero/Hero.jsx
+++ portfolio/src/components/hero/Hero.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+// import React from 'react'
 import { useGlobalContext } from '../../hooks/context'
 import { FaRegPaperPlane } from 'react-icons/fa'
-import image from '../../assets/div.jpeg'
+// import image from '../../assets/div.jpeg'
 const Hero = () => {
   const { social } = useGlobalContext()
   return (
-    <section className='mx-4 mt-20 md:mx-16 '>
-      <div className='flex justify-between lg:gap-16 md:gap-8 '>
-        <ul className='hidden md:flex  sm:relative  fixed top-16  gap-4 flex-col  lg:my-36 '>
+    <section className='mx-4 mt-20 md:mx-16 sm:mx-4'>
+      <div className='flex justify-between lg:gap-16 sm:gap-8 '>
+        <ul className='hidden sm:flex  sm:relative  fixed top-16  gap-4 flex-col  lg:my-36 '>
           {social.map((link) => {
             const { id, url, icon } = link
             return (
-              <li key={id} className='hover:animate-bounce hover:rotate-45 '>
+              <li key={id} className='hover:animate-bounce  '>
                 <a
                   href={url}
                   target='_blank'
@@ -23,22 +23,26 @@ const Hero = () => {
             )
           })}
         </ul>
-        <div className='flex flex-col-reverse md:flex-row items-center lg:justify-between justify-center mx-auto bg-red-30   w-full  '>
-          <article className='flex flex-col gap-4 w-[100%] lg:w-[45%] '>
-            <h1 className='text-5xl'>Hi,I am Divine</h1>
-            <h3 className='text-3xl'>Frontend developer</h3>
-            <p className='text-md'>
+        <div className='flex flex-col-reverse sm:flex-row items-center lg:justify-between justify-center mx-auto bg-red-30   w-full  '>
+          <article className='flex flex-col gap-4 sm:gap-4 w-[100%] lg:w-[45%] '>
+            <h1 className='lg:text-5xl mt-3 tracking-wide leading-6 sm:mt-0 text-center sm:text-left text-3xl sm:text-3xl'>
+              Hi,I am Divine
+            </h1>
+            <h3 className='lg:text-3xl tracking-wide leading-6 text-xl sm:text-2xl text-center sm:text-left'>
+              Frontend developer
+            </h3>
+            <p className='sm:text-md md:text-md tracking-wide leading-6 text-sm sm:text-left text-center'>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus
               harum perspiciatis et minima rerum facilis quia. Possimus
               praesentium laudantium vitae dolor nisi harum quia porro quaerat
               doloribus. Aperiam, voluptate ipsa.
             </p>
-            <button className='p-4 mt-4 items-center rounded-md capitalize justify-center font-bold flex gap-2 text-white bg-blue-300 w-[150px]'>
+            <button className='p-4 mt-4 items-center rounded-md capitalize justify-center font-bold flex gap-2 text-white bg-blue-300 w-[150px] mx-auto sm:mx-0 '>
               contact me <FaRegPaperPlane />
             </button>
           </article>
           <div className='flex lg:w-[40%] w-[100%] justify-center '>
-            <ul className='md:hidden  flex  absolute left-4  top-16 sm:top-28  gap-4 flex-col  '>
+            <ul className='sm:hidden  flex  absolute left-4  top-16 sm:top-28  gap-4 flex-col  '>
               {social.map((link) => {
                 const { id, url, icon } = link
                 return (
@@ -55,8 +59,8 @@ const Hero = () => {
               })}
             </ul>
             <div
-              className=' rounded-b-full  rounded-t-full h-[170px] w-[170px]
-              sm:h-[180px] sm:w-[180px]
+              className=' rounded-b-full  rounded-t-full h-[180px] w-[180px]
+              sm:h-[210px] sm:w-[210px] 
             md:h-[240px] md:w-[240px]   bg-hero lg:h-[300px] lg:w-[300px] xl:h-[320px] xl:w-[320px] bg-center bg-cover'
             >
               {/* <img className='' src={image} alt='' /> */}
